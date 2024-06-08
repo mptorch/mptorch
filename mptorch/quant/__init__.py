db93348285dcab9533d6b746e58985a94aeba4ea
Silviu Filip <silviu.ioan.filip@gmail.com>
2024-06-08 16:14:01 -0700

    python interface for SuperNormalFloats

--- mptorch/quant/__init__.py
+++ mptorch/quant/__init__.py
@@ -2,13 +2,13 @@ from .quant_function import *
 from .quant_module import *
 from .quant_format import QAffineFormats
 from .modules import *
-from . import functional
 from .functional import qmatmul
 
 __all__ = [
     "fixed_point_quantize",
     "block_quantize",
     "float_quantize",
+    "superfp_quantize",
     "quantizer",
     "QAvgPool2d",
     "QAffineFormats",
