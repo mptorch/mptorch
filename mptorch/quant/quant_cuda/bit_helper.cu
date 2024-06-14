042592e2fb66b8cda06e7d26210f77b792b3ae70
Victor Ravain <victorravain@hotmail.fr>
2024-06-14 16:47:09 -0700

    [DEV] Add the cast_p3109_unsigned_nearest function [FIX] add our own clip_exponent function to restore the original one to not break other code

--- mptorch/quant/quant_cuda/bit_helper.cu
+++ mptorch/quant/quant_cuda/bit_helper.cu
@@ -16,8 +16,7 @@ __device__ __forceinline__ uint32_t extract_exponent(float *a) {
 }
 
 __device__ __forceinline__ uint32_t
-round_bitwise_stochastic(uint32_t target, uint32_t rand_prob,
-                         int man_bits) {
+round_bitwise_stochastic(uint32_t target, uint32_t rand_prob, int man_bits) {
   uint32_t mask = (1 << (23 - man_bits)) - 1;
   uint32_t add_r = target + (rand_prob & mask);
   uint32_t quantized = add_r & ~mask;
@@ -34,8 +33,7 @@ round_bitwise_stochastic(uint32_t target, uint32_t rand_prob,
 // in this case we round down because we are in the middle with the nearest even being the down
 
 __device__ __forceinline__ uint32_t
-round_bitwise_nearest(uint32_t target, int old_man_bits, int subnormal_shift) {
-  int man_bits = old_man_bits - subnormal_shift;
+round_bitwise_nearest(uint32_t target, int man_bits) {
   uint32_t down = target << (8 + man_bits) >> (8 + man_bits);
   uint32_t machine_eps = 1 << (22 - man_bits);
   // tie breaking rule offset
@@ -70,7 +68,56 @@ __device__ __forceinline__ uint32_t round_bitwise_down(uint32_t target, int man_
 }
 
 __device__ __forceinline__ uint32_t
-uint32_t clip_exponent(int exp_bits, int man_bits, uint32_t old_num, uint32_t quantized_num, bool saturate, bool subnormal) {
+clip_exponent(int exp_bits, int man_bits, uint32_t old_num,
+              uint32_t quantized_num, bool saturate = false) {
+  if (quantized_num == 0)
+    return quantized_num;
+
+  int quantized_exponent_store = quantized_num << 1 >> 24;
+  int max_exponent_store = (1 << (exp_bits - 1)) - 1 + 127;
+  int min_exponent_store = -((1 << (exp_bits - 1)) - 2) + 127;
+
+  uint32_t old_sign = old_num >> 31 << 31;
+  // saturate or overflow
+  if (quantized_exponent_store > max_exponent_store) {
+    if (saturate) {
+      uint32_t max_man =
+          (uint32_t)-1 << 9 >> 9 >> (23 - man_bits) << (23 - man_bits);
+      uint32_t max_num = ((uint32_t)max_exponent_store << 23) | max_man;
+      quantized_num = old_sign | max_num;
+    } else {
+      quantized_num =
+          ((((uint32_t)1 << 31) - 1) ^ (((uint32_t)1 << 23) - 1));
+      quantized_num = quantized_num | old_sign;
+    }
+  } else if (quantized_exponent_store < min_exponent_store) {
+    uint32_t min_num = ((uint32_t)min_exponent_store << 23);
+    uint32_t middle_num = ((uint32_t)(min_exponent_store - 1) << 23);
+    uint32_t unsigned_quantized_num = quantized_num << 1 >> 1;
+    if (unsigned_quantized_num > middle_num) {
+      uint32_t old_sign = old_num >> 31 << 31;
+      quantized_num = old_sign | min_num;
+    } else {
+      quantized_num = 0;
+    }
+  }
+  return quantized_num;
+}
+
+__device__ __forceinline__ uint32_t
+clip_max_exponent(int man_bits, uint32_t max_exponent,  uint32_t quantized_num) {
+  uint32_t quantized_exponent = quantized_num << 1 >> 24 << 23; // 1 sign bit, 23 mantissa bits
+  if (quantized_exponent > max_exponent) {
+    uint32_t max_man = (uint32_t)-1 << 9 >> 9 >> (23 - man_bits) << (23 - man_bits); // 1 sign bit, 8 exponent bits
+    uint32_t max_num = max_exponent | max_man;
+    uint32_t old_sign = quantized_num >> 31 << 31;
+    quantized_num = old_sign | max_num;
+  }
+  return quantized_num;
+}
+
+__device__ __forceinline__ uint32_t
+p3109_clip_exponent(int exp_bits, int man_bits, uint32_t old_num, uint32_t quantized_num, bool saturate, bool subnormal) {
   if (quantized_num == 0) {
     return quantized_num;
   }
@@ -113,17 +160,5 @@ uint32_t clip_exponent(int exp_bits, int man_bits, uint32_t old_num, uint32_t qu
     }
   }
 
-  return quantized_num;
-}
-
-__device__ __forceinline__ uint32_t
-clip_max_exponent(int man_bits, uint32_t max_exponent,  uint32_t quantized_num) {
-  uint32_t quantized_exponent = quantized_num << 1 >> 24 << 23; // 1 sign bit, 23 mantissa bits
-  if (quantized_exponent > max_exponent) {
-    uint32_t max_man = (uint32_t)-1 << 9 >> 9 >> (23 - man_bits) << (23 - man_bits); // 1 sign bit, 8 exponent bits
-    uint32_t max_num = max_exponent | max_man;
-    uint32_t old_sign = quantized_num >> 31 << 31;
-    quantized_num = old_sign | max_num;
-  }
   return quantized_num;
 }
--- mptorch/quant/quant_cuda/p3109_kernel.cu
+++ mptorch/quant/quant_cuda/p3109_kernel.cu
@@ -32,8 +32,8 @@ __device__ float cast_p3109_signed_nearest(float origin_float, int P) {
       subnormal_shift = min_exp - exp_val;
     }
 
-    uval8 = round_bitwise_nearest(uval32, man_bits, subnormal_shift);
-    uval8 = clip_exponent(exp_bits, man_bits, uval32, uval8, true, subnormals);
+    uval8 = round_bitwise_nearest(uval32, man_bits - subnormal_shift);
+    uval8 = p3109_clip_exponent(exp_bits, man_bits, uval32, uval8, true, subnormals);
     fval8 = BITS_TO_FLOAT(&uval8);
 
     return fval8;
@@ -61,8 +61,8 @@ __device__ float cast_p3109_signed_nearest<false>(float origin_float, int P) {
         return origin_float;
     }
 
-    uval8 = round_bitwise_nearest(uval32,man_bits, 0);
-    uval8 = clip_exponent(exp_bits, man_bits, uval32, uval8, true, subnormals);
+    uval8 = round_bitwise_nearest(uval32,man_bits - subnormal_shift);
+    uval8 = p3109_clip_exponent(exp_bits, man_bits, uval32, uval8, true, subnormals);
     fval8 = BITS_TO_FLOAT(&uval8);
 
     return fval8;
@@ -82,14 +82,86 @@ __device__ float cast_p3109_signed_stochastic<false>(float origin_float, int P, 
 
 template <bool subnormals>
 __device__ float cast_p3109_unsigned_nearest(float origin_float, int P) {
-  // TODO:
-  return 0.0f;
+    // P range from 1 to 7 in signed
+    int exp_bits = 8 - P + 1;
+    int man_bits = P - 1; 
+    
+    int spec_exp = (P == 1) ? 1 : 0;
+
+    uint32_t uval32, uval8;
+    float fval8;
+    uval32 = FLOAT_TO_BITS(&origin_float);
+
+
+    int sign = uval32 >> 31;
+    int exp_val = (uval32 << 1 >> 24) - 127;
+    
+    bool subnormals = true;
+
+    // minimal and maximal exponent value in binary8
+    int max_exp = (1 << (exp_bits -1)) - 1;
+    int min_exp = spec_exp - max_exp;
+    
+    if(sign == 1){
+        return NAN;
+    }
+
+    if (exp_val == 128) {             // inf/Nan case
+        return origin_float;
+    }
+    
+    int subnormal_shift = 0;
+    if((min_exp - exp_val) <= man_bits && exp_val < min_exp && subnormals){ 
+      subnormal_shift = min_exp - exp_val;
+    }
+
+    uval8 = round_bitwise_nearest(uval32, man_bits - subnormal_shift);
+    uval8 = p3109_clip_exponent(exp_bits, man_bits, uval32, uval8, true, subnormals);
+    fval8 = BITS_TO_FLOAT(&uval8);
+
+    return fval8;
 }
 
 template <>
 __device__ float cast_p3109_unsigned_nearest<false>(float origin_float, int P) {
-  // TODO:
-  return 0.0f;
+    // P range from 1 to 7 in signed
+    int exp_bits = 8 - P + 1;
+    int man_bits = P - 1; 
+    
+    int spec_exp = (P == 1) ? 1 : 0;
+
+    uint32_t uval32, uval8;
+    float fval8;
+    uval32 = FLOAT_TO_BITS(&origin_float);
+
+
+    int sign = uval32 >> 31;
+    int exp_val = (uval32 << 1 >> 24) - 127;
+    
+    bool subnormals = false;
+
+    // minimal and maximal exponent value in binary8
+    int max_exp = (1 << (exp_bits -1)) - 1;
+    int min_exp = spec_exp - max_exp;
+    
+    if(sign == 1){
+        return NAN;
+    }
+
+    if (exp_val == 128) {             // inf/Nan case
+        return origin_float;
+    }
+    
+    int subnormal_shift = 0;
+    if((min_exp - exp_val) <= man_bits && exp_val < min_exp && subnormals){ 
+      subnormal_shift = min_exp - exp_val;
+    }
+
+    uval8 = round_bitwise_nearest(uval32, man_bits - subnormal_shift);
+    uval8 = p3109_clip_exponent(exp_bits, man_bits, uval32, uval8, true, subnormals);
+    fval8 = BITS_TO_FLOAT(&uval8);
+
+    return fval8;
 }
 
 template <bool subnormals>
@@ -104,9 +176,15 @@ __device__ float cast_p3109_unsigned_stochastic<false>(float origin_float, int P
   return 0.0f;
 }
 
-__global__ void p3109_signed_kernel_nearest(float *__restrict__ a, float *o, int size,
-                                      int P, bool subnormals) {
-  // TODO
+__global__ void p3109_signed_kernel_nearest(float *__restrict__ a, float *o, int size, int P, bool subnormals) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  if (idx < size) {
+      if (subnormals) {
+          o[idx] = cast_p3109_signed_nearest<true>(a[idx], P);
+      } else {
+          o[idx] = cast_p3109_signed_nearest<false>(a[idx], P);
+      }
+  }
 }
 
 __global__ void p3109_unsigned_kernel_nearest(float *__restrict__ a, float *o, int size,
