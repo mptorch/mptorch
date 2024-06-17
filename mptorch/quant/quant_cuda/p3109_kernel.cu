c239d582b3100b1155c73b06a39cf1b9c5f27ddd
Victor Ravain <victorravain@hotmail.fr>
2024-06-17 16:28:59 -0700

    [DEV] Develop the signed and unsigned stochastic rounding function

--- mptorch/quant/quant_cuda/p3109_kernel.cu
+++ mptorch/quant/quant_cuda/p3109_kernel.cu
@@ -70,14 +70,96 @@ __device__ float cast_p3109_signed_nearest<false>(float origin_float, int P) {
 
 template <bool subnormals>
 __device__ float cast_p3109_signed_stochastic(float origin_float, int P, int prng_bits) {
-  // TODO:
-  return 0.0f;
+
+    int exp_bits = 8-P;
+    int man_bits = P-1;  
+    
+    int spec_exp = (P == 1) ? 1 : 0;
+    
+    bool subnormals = true;
+
+    uint32_t uval32, uval8;
+    float fval8;
+    uval32 = FLOAT_TO_BITS(&origin_float);
+
+    int exp_val = (uval32 << 1 >> 24) - 127;
+
+    // minimal and maximal exponent value in binary8
+    int max_exp = (1 << (exp_bits -1)) - 1;
+    int min_exp = spec_exp - max_exp;
+    
+        
+    int subnormal_shift = 0;
+    if(((min_exp - exp_val) <= man_bits) && (exp_val < min_exp) && (subnormals)){ 
+      subnormal_shift = min_exp - exp_val;
+    }
+    
+    // Calculate the upper limit of the rand_prob
+    uint64_t upper_limit = static_cast<uint32_t>(std::pow(2, 23 - (man_bits - subnormal_shift))) - 1;
+
+    // Random number generator
+    std::random_device rd; // Seed
+    std::mt19937 gen(rd()); // Mersenne Twister engine
+
+    // Distribution that covers the range [0, 2^x - 1]
+    std::uniform_int_distribution<uint32_t> distrib(0, upper_limit);
+
+    // Generate the random number
+    uint64_t random_number = distrib(gen);
+
+    if (exp_val == 128) {             // inf/Nan case
+        return origin_float;
+    }
+
+    uval8 = round_bitwise_stochastic(uval32, random_number, man_bits - subnormal_shift);
+    uval8 = p3109_clip_exponent(exp_bits, man_bits, uval32, uval8, true, subnormals);
+    fval8 = BITS_TO_FLOAT(&uval8);
+
+    return fval8;
 }
 
 template <>
 __device__ float cast_p3109_signed_stochastic<false>(float origin_float, int P, int prng_bits) {
-  // TODO:
-  return 0.0f;
+
+    int exp_bits = 8-P;
+    int man_bits = P-1;  
+    
+    int spec_exp = (P == 1) ? 1 : 0;
+    
+    bool subnormals = false;
+
+    uint32_t uval32, uval8;
+    float fval8;
+    uval32 = FLOAT_TO_BITS(&origin_float);
+
+    int exp_val = (uval32 << 1 >> 24) - 127;
+
+    // minimal and maximal exponent value in binary8
+    int max_exp = (1 << (exp_bits -1)) - 1;
+    int min_exp = spec_exp - max_exp;
+    
+    // Calculate the upper limit of the rand_prob
+    uint64_t upper_limit = static_cast<uint32_t>(std::pow(2, 23 - man_bits)) - 1;
+
+    // Random number generator
+    std::random_device rd; // Seed
+    std::mt19937 gen(rd()); // Mersenne Twister engine
+
+    // Distribution that covers the range [0, 2^x - 1]
+    std::uniform_int_distribution<uint32_t> distrib(0, upper_limit);
+
+    // Generate the random number
+    uint64_t random_number = distrib(gen);
+
+    if (exp_val == 128) {             // inf/Nan case
+        return origin_float;
+    }
+
+    uval8 = round_bitwise_stochastic(uval32, random_number, man_bits);
+    uval8 = p3109_clip_exponent(exp_bits, man_bits, uval32, uval8, true, subnormals);
+    fval8 = BITS_TO_FLOAT(&uval8);
+
+    return fval8;
 }
 
 template <bool subnormals>
@@ -86,7 +168,7 @@ __device__ float cast_p3109_unsigned_nearest(float origin_float, int P) {
     if (origin_float < 0){
       return NAN;
     }
-    // P range from 1 to 7 in signed
+ 
     int exp_bits = 8 - P + 1;
     int man_bits = P - 1; 
     
@@ -125,7 +207,6 @@ __device__ float cast_p3109_unsigned_nearest(float origin_float, int P) {
 
 template <>
 __device__ float cast_p3109_unsigned_nearest<false>(float origin_float, int P) {
-    // P range from 1 to 7 in signed
 
     if (origin_float < 0){
       return NAN;
@@ -154,13 +235,8 @@ __device__ float cast_p3109_unsigned_nearest<false>(float origin_float, int P) {
     if (exp_val == 128) {             // inf/Nan case
         return origin_float;
     }
-    
-    int subnormal_shift = 0;
-    if((min_exp - exp_val) <= man_bits && exp_val < min_exp && subnormals){ 
-      subnormal_shift = min_exp - exp_val;
-    }
 
-    uval8 = round_bitwise_nearest(uval32, man_bits - subnormal_shift);
+    uval8 = round_bitwise_nearest(uval32, man_bits);
     uval8 = p3109_clip_exponent(exp_bits, man_bits, uval32, uval8, true, subnormals);
     fval8 = BITS_TO_FLOAT(&uval8);
 
@@ -169,14 +245,109 @@ __device__ float cast_p3109_unsigned_nearest<false>(float origin_float, int P) {
 
 template <bool subnormals>
 __device__ float cast_p3109_unsigned_stochastic(float origin_float, int P, int prng_bits) { // what is prng_bits? number of random bits?
-  // TODO:
-  return 0.0f;
+    
+    if(origin_float < 0){
+        return NAN;
+    }
+
+    // P range from 1 to 7 in signed
+    int exp_bits = 8 - P + 1;
+    int man_bits = P - 1; 
+    
+    int spec_exp = (P == 1) ? 1 : 0;
+    
+    bool subnormals = true;
+
+    uint32_t uval32, uval8;
+    float fval8;
+    uval32 = FLOAT_TO_BITS(&origin_float);
+
+    int exp_val = (uval32 << 1 >> 24) - 127;
+
+    // minimal and maximal exponent value in binary8
+    int max_exp = (1 << (exp_bits -1)) - 1;
+    int min_exp = spec_exp - max_exp;
+        
+    int subnormal_shift = 0;
+    if(((min_exp - exp_val) <= man_bits) && (exp_val < min_exp) && (subnormals)){ 
+      subnormal_shift = min_exp - exp_val;
+    }
+    
+    // Calculate the upper limit of the rand_prob
+    uint64_t upper_limit = static_cast<uint32_t>(std::pow(2, 23 - (man_bits - subnormal_shift))) - 1;
+
+    // Random number generator
+    std::random_device rd; // Seed
+    std::mt19937 gen(rd()); // Mersenne Twister engine
+
+    // Distribution that covers the range [0, 2^x - 1]
+    std::uniform_int_distribution<uint32_t> distrib(0, upper_limit);
+
+    // Generate the random number
+    uint64_t random_number = distrib(gen);
+
+    if (exp_val == 128) {             // inf/Nan case
+        return origin_float;
+    }
+
+    uval8 = round_bitwise_stochastic(uval32, random_number, man_bits - subnormal_shift);
+    uval8 = p3109_clip_exponent(exp_bits, man_bits, uval32, uval8, true, subnormals);
+    fval8 = BITS_TO_FLOAT(&uval8);
+
+    return fval8;
 }
 
 template <>
 __device__ float cast_p3109_unsigned_stochastic<false>(float origin_float, int P, int prng_bits) {
-  // TODO:
-  return 0.0f;
+
+  if (origin_float < 0){
+    return NAN;
+  }
+
+   // P range from 1 to 7 in signed
+    int exp_bits = 8 - P + 1;
+    int man_bits = P - 1; 
+    
+    int spec_exp = (P == 1) ? 1 : 0;
+    
+    bool subnormals = false;
+
+    uint32_t uval32, uval8;
+    float fval8;
+    uval32 = FLOAT_TO_BITS(&origin_float);
+
+    int exp_val = (uval32 << 1 >> 24) - 127;
+
+    // minimal and maximal exponent value in binary8
+    int max_exp = (1 << (exp_bits -1)) - 1;
+    int min_exp = spec_exp - max_exp;
+
+    if(sign == 1){
+        return NAN;
+    }
+    
+    // Calculate the upper limit of the rand_prob
+    uint64_t upper_limit = static_cast<uint32_t>(std::pow(2, 23 - man_bits)) - 1;
+
+    // Random number generator
+    std::random_device rd; // Seed
+    std::mt19937 gen(rd()); // Mersenne Twister engine
+
+    // Distribution that covers the range [0, 2^x - 1]
+    std::uniform_int_distribution<uint32_t> distrib(0, upper_limit);
+
+    // Generate the random number
+    uint64_t random_number = distrib(gen);
+
+    if (exp_val == 128) {             // inf/Nan case
+        return origin_float;
+    }
+
+    uval8 = round_bitwise_stochastic(uval32, random_number, man_bits);
+    uval8 = p3109_clip_exponent(exp_bits, man_bits, uval32, uval8, true, subnormals);
+    fval8 = BITS_TO_FLOAT(&uval8);
+
+    return fval8;
 }
 
 __global__ void p3109_signed_kernel_nearest(float *__restrict__ a, float *o, int size, int P, bool subnormals) {
