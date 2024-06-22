7204d39be192245edbf300001753b25f8f0b07b0
Victor <victorravain@hotmail.fr>
2024-06-21 17:05:06 -0700

    [FIX] adapt this test script to the simplification made in p3109_kernel.cu

--- dev/cuda/p3109_signed_nearest.cu
+++ dev/cuda/p3109_signed_nearest.cu
@@ -95,33 +95,30 @@ uint32_t p3109_clip_exponent_cpu(int exp_bits, int man_bits, uint32_t old_num, u
   return quantized_num;
 }
 
-template<bool subnormals>
-float cast_p3109_signed_nearest_cpu(float origin_float, int P) {
-    // P range from 1 to 7 in signed
-    int exp_bits = 8-P;
-    int man_bits = P-1; //P in unsigned
-    
-    int spec_exp = (P == 1) ? 1 : 0;
+float cast_p3109_signed_nearest_cpu(float origin_float, int P, bool subnormals) {
 
+    int exp_bits = 8-P;
+    int man_bits = P-1;    
+    int subnormal_shift = 0;
     uint32_t uval32, uval8;
     float fval8;
+
     uval32 = FLOAT_TO_BITS(&origin_float);
 
     int exp_val = (uval32 << 1 >> 24) - 127;
 
-    // bool subnormals = true;
-
-    // minimal and maximal exponent value in binary8
-    int max_exp = (1 << (exp_bits -1)) - 1;
-    int min_exp = spec_exp - max_exp;
-
     if (exp_val == 128) {             // inf/Nan case
         return origin_float;
     }
-    
-    int subnormal_shift = 0;
-    if(((min_exp - exp_val) <= man_bits) && (exp_val < min_exp) && (subnormals)){ 
-      subnormal_shift = min_exp - exp_val;
+
+    if (subnormals){
+        int spec_exp = (P == 1) ? 1 : 0;
+        int max_exp = (1 << (exp_bits -1)) - 1;    // minimal and maximal exponent value in binary8
+        int min_exp = spec_exp - max_exp;
+
+        if(((min_exp - exp_val) <= man_bits) && (exp_val < min_exp) && (subnormals)){ 
+            subnormal_shift = min_exp - exp_val;
+        }
     }
 
     uval8 = round_bitwise_nearest_cpu(uval32, man_bits - subnormal_shift);
@@ -131,106 +128,44 @@ float cast_p3109_signed_nearest_cpu(float origin_float, int P) {
     return fval8;
 }
 
-template<>
-float cast_p3109_signed_nearest_cpu<false>(float origin_float, int P) {
-    // P range from 1 to 7 in signed
-    int exp_bits = 8-P;
-    int man_bits = P-1; //P in unsigned
+float cast_p3109_unsigned_nearest_cpu(float origin_float, int P, bool subnormals) {
+    // we had talks abt the following for unsigned, P = 1:
+    // 0: 0000 0000
+    // NaN: FE or FF (currently. it is 1000 0000 in this revision)
+    // inf: FE of FF (currently, it is FF)
+    // max float: FD (currently, it is FE)
+    // we have a variation that allows for this in the special condition where 
     
-    int spec_exp = (P == 1) ? 1 : 0;
-
-    uint32_t uval32, uval8;
-    float fval8;
-    uval32 = FLOAT_TO_BITS(&origin_float);
-
-    int exp_val = (uval32 << 1 >> 24) - 127;
-
-    // minimal and maximal exponent value in binary8
-    int max_exp = (1 << (exp_bits -1)) - 1;
-    int min_exp = spec_exp - max_exp;
-
-    if (exp_val == 128) {             // inf/Nan case
-        return origin_float;
-    }
-
-    uval8 = round_bitwise_nearest_cpu(uval32, man_bits);
-    uval8 = p3109_clip_exponent_cpu(exp_bits, man_bits, uval32, uval8, true, false);
-    fval8 = BITS_TO_FLOAT(&uval8);
-
-    return fval8;
-}
-
-template <bool subnormals>
-float cast_p3109_unsigned_nearest_cpu(float origin_float, int P) {
-
     if (origin_float < 0){
       return NAN;
     }
  
     int exp_bits = 8 - P + 1;
     int man_bits = P - 1; 
-    
-    int spec_exp = (P == 1) ? 1 : 0;
-
+    int subnormal_shift = 0;
     uint32_t uval32, uval8;
     float fval8;
+
     uval32 = FLOAT_TO_BITS(&origin_float);
 
-
-    int sign = uval32 >> 31;
     int exp_val = (uval32 << 1 >> 24) - 127;
     
-    // bool subnormals = true;
-
-    // minimal and maximal exponent value in binary8
-    int max_exp = (1 << (exp_bits -1)) - 1;
-    int min_exp = spec_exp - max_exp;
-    
-
-    if (exp_val == 128) {             // inf/Nan case
+    if (exp_val == 128) {   
         return origin_float;
     }
-    
-    int subnormal_shift = 0;
-    if((min_exp - exp_val) <= man_bits && exp_val < min_exp && subnormals){ 
-      subnormal_shift = min_exp - exp_val;
+
+    if (subnormals){
+        int spec_exp = (P == 1) ? 1 : 0;
+        int max_exp = (1 << (exp_bits -1)) - 1;
+        int min_exp = spec_exp - max_exp;
+           
+        if((min_exp - exp_val) <= man_bits && exp_val < min_exp && subnormals){ 
+            subnormal_shift = min_exp - exp_val;
+        }
     }
 
     uval8 = round_bitwise_nearest_cpu(uval32, man_bits - subnormal_shift);
     uval8 = p3109_clip_exponent_cpu(exp_bits, man_bits, uval32, uval8, true, subnormals);
-    fval8 = BITS_TO_FLOAT(&uval8);
-
-    return fval8;
-}
-
-template <>
-float cast_p3109_unsigned_nearest_cpu<false>(float origin_float, int P) {
-
-    if (origin_float < 0){
-      return NAN;
-    }
-    
-    int exp_bits = 8 - P + 1;
-    int man_bits = P - 1; 
-    
-    int spec_exp = (P == 1) ? 1 : 0;
-
-    uint32_t uval32, uval8;
-    float fval8;
-    uval32 = FLOAT_TO_BITS(&origin_float);
-
-    int exp_val = (uval32 << 1 >> 24) - 127;
-
-    // maximal exponent value in binary8
-    int max_exp = (1 << (exp_bits -1)) - 1;
-
-
-    if (exp_val == 128) {             // inf/Nan case
-        return origin_float;
-    }
-
-    uval8 = round_bitwise_nearest_cpu(uval32, man_bits);
-    uval8 = p3109_clip_exponent_cpu(exp_bits, man_bits, uval32, uval8, true, false);
     fval8 = BITS_TO_FLOAT(&uval8);
 
     return fval8;
@@ -239,18 +174,10 @@ float cast_p3109_unsigned_nearest_cpu<false>(float origin_float, int P) {
 void p3109_signed_nearest_cpu(float *o, float *a, int N, int P, bool is_signed, bool subnormals) {
   for (int i = 0; i < N; ++i){
     if(is_signed){
-      if(subnormals){
-        o[i] = cast_p3109_signed_nearest_cpu<true>(a[i], P);
-      }else{
-        o[i] = cast_p3109_signed_nearest_cpu<false>(a[i], P);
-      }
+      o[i] = cast_p3109_signed_nearest_cpu(a[i], P, subnormals);
     }
     else{
-      if(subnormals){
-        o[i] = cast_p3109_unsigned_nearest_cpu<true>(a[i], P);
-      }else{
-        o[i] = cast_p3109_unsigned_nearest_cpu<false>(a[i], P);
-      }
+      o[i] = cast_p3109_unsigned_nearest_cpu(a[i], P, subnormals);
     }
   }
 }
@@ -319,33 +246,30 @@ p3109_clip_exponent_gpu(int exp_bits, int man_bits, uint32_t old_num, uint32_t q
   return quantized_num;
 }
 
-template<bool subnormals>
-__device__ float cast_p3109_signed_nearest_gpu(float origin_float, int P) {
-    // P range from 1 to 7 in signed
-    int exp_bits = 8-P;
-    int man_bits = P-1; //P in unsigned
-    
-    int spec_exp = (P == 1) ? 1 : 0;
+__device__ float cast_p3109_signed_nearest_gpu(float origin_float, int P, bool subnormals) {
 
+    int exp_bits = 8-P;
+    int man_bits = P-1;    
+    int subnormal_shift = 0;
     uint32_t uval32, uval8;
     float fval8;
+
     uval32 = FLOAT_TO_BITS(&origin_float);
 
-    // bool subnormals = true;
-
     int exp_val = (uval32 << 1 >> 24) - 127;
-
-    // minimal and maximal exponent value in binary8
-    int max_exp = (1 << (exp_bits -1)) - 1;
-    int min_exp = spec_exp - max_exp;
 
     if (exp_val == 128) {             // inf/Nan case
         return origin_float;
     }
-    
-    int subnormal_shift = 0;
-    if(((min_exp - exp_val) <= man_bits) && (exp_val < min_exp) && (subnormals)){ 
-      subnormal_shift = min_exp - exp_val;
+
+    if (subnormals){
+        int spec_exp = (P == 1) ? 1 : 0;
+        int max_exp = (1 << (exp_bits -1)) - 1;    // minimal and maximal exponent value in binary8
+        int min_exp = spec_exp - max_exp;
+
+        if(((min_exp - exp_val) <= man_bits) && (exp_val < min_exp) && (subnormals)){ 
+            subnormal_shift = min_exp - exp_val;
+        }
     }
 
     uval8 = round_bitwise_nearest_gpu(uval32, man_bits - subnormal_shift);
@@ -355,120 +279,44 @@ __device__ float cast_p3109_signed_nearest_gpu(float origin_float, int P) {
     return fval8;
 }
 
-template<>
-__device__ float cast_p3109_signed_nearest_gpu<false>(float origin_float, int P) {
-
-    if (origin_float < 0){
-      return NAN;
-    }
+__device__ float cast_p3109_unsigned_nearest_gpu(float origin_float, int P, bool subnormals) {
+    // we had talks abt the following for unsigned, P = 1:
+    // 0: 0000 0000
+    // NaN: FE or FF (currently. it is 1000 0000 in this revision)
+    // inf: FE of FF (currently, it is FF)
+    // max float: FD (currently, it is FE)
+    // we have a variation that allows for this in the special condition where 
     
-    int exp_bits = 8 - P + 1;
-    int man_bits = P - 1; 
-    
-    int spec_exp = (P == 1) ? 1 : 0;
-
-    uint32_t uval32, uval8;
-    float fval8;
-    uval32 = FLOAT_TO_BITS(&origin_float);
-
-
-    int sign = uval32 >> 31;
-    int exp_val = (uval32 << 1 >> 24) - 127;
-    
-    // bool subnormals = false;
-
-    // minimal and maximal exponent value in binary8
-    int max_exp = (1 << (exp_bits -1)) - 1;
-    int min_exp = spec_exp - max_exp;
-
-
-    if (exp_val == 128) {             // inf/Nan case
-        return origin_float;
-    }
-
-    uval8 = round_bitwise_nearest_gpu(uval32, man_bits);
-    uval8 = p3109_clip_exponent_gpu(exp_bits, man_bits, uval32, uval8, true, false);
-    fval8 = BITS_TO_FLOAT(&uval8);
-
-    return fval8;
-}
-
-template <bool subnormals>
-__device__ float cast_p3109_unsigned_nearest_gpu(float origin_float, int P) {
-
     if (origin_float < 0){
       return NAN;
     }
  
     int exp_bits = 8 - P + 1;
     int man_bits = P - 1; 
-    
-    int spec_exp = (P == 1) ? 1 : 0;
-
+    int subnormal_shift = 0;
     uint32_t uval32, uval8;
     float fval8;
+
     uval32 = FLOAT_TO_BITS(&origin_float);
 
-
-    int sign = uval32 >> 31;
     int exp_val = (uval32 << 1 >> 24) - 127;
     
-  // bool subnormals = true;
-
-    // minimal and maximal exponent value in binary8
-    int max_exp = (1 << (exp_bits -1)) - 1;
-    int min_exp = spec_exp - max_exp;
-    
-
-    if (exp_val == 128) {             // inf/Nan case
+    if (exp_val == 128) {   
         return origin_float;
     }
-    
-    int subnormal_shift = 0;
-    if((min_exp - exp_val) <= man_bits && exp_val < min_exp && subnormals){ 
-      subnormal_shift = min_exp - exp_val;
+
+    if (subnormals){
+        int spec_exp = (P == 1) ? 1 : 0;
+        int max_exp = (1 << (exp_bits -1)) - 1;
+        int min_exp = spec_exp - max_exp;
+           
+        if((min_exp - exp_val) <= man_bits && exp_val < min_exp && subnormals){ 
+            subnormal_shift = min_exp - exp_val;
+        }
     }
 
     uval8 = round_bitwise_nearest_gpu(uval32, man_bits - subnormal_shift);
     uval8 = p3109_clip_exponent_gpu(exp_bits, man_bits, uval32, uval8, true, subnormals);
-    fval8 = BITS_TO_FLOAT(&uval8);
-
-    return fval8;
-}
-
-template <>
-__device__ float cast_p3109_unsigned_nearest_gpu<false>(float origin_float, int P) {
-
-    if (origin_float < 0){
-      return NAN;
-    }
-    
-    int exp_bits = 8 - P + 1;
-    int man_bits = P - 1; 
-    
-    int spec_exp = (P == 1) ? 1 : 0;
-
-    uint32_t uval32, uval8;
-    float fval8;
-    uval32 = FLOAT_TO_BITS(&origin_float);
-
-
-    int sign = uval32 >> 31;
-    int exp_val = (uval32 << 1 >> 24) - 127;
-    
-    // bool subnormals = false;
-
-    // minimal and maximal exponent value in binary8
-    int max_exp = (1 << (exp_bits -1)) - 1;
-    int min_exp = spec_exp - max_exp;
-
-
-    if (exp_val == 128) {             // inf/Nan case
-        return origin_float;
-    }
-
-    uval8 = round_bitwise_nearest_gpu(uval32, man_bits);
-    uval8 = p3109_clip_exponent_gpu(exp_bits, man_bits, uval32, uval8, true, false);
     fval8 = BITS_TO_FLOAT(&uval8);
 
     return fval8;
@@ -480,17 +328,9 @@ __global__ void p3109_signed_nearest_gpu(float *o, float *__restrict__ a, int N,
     if (index < N)
     {
       if(is_signed){
-        if(subnormals){
-          o[index] = cast_p3109_signed_nearest_gpu<true>(a[index], P);
-        }else{
-          o[index] = cast_p3109_signed_nearest_gpu<false>(a[index], P);
-        }
+        o[index] = cast_p3109_signed_nearest_gpu(a[index], P, subnormals);
       }else{
-        if(subnormals){
-          o[index] = cast_p3109_unsigned_nearest_gpu<true>(a[index], P);
-        }else{
-          o[index] = cast_p3109_unsigned_nearest_gpu<false>(a[index], P);
-        }
+        o[index] = cast_p3109_unsigned_nearest_gpu(a[index], P, subnormals);
       }
     }
 }
@@ -549,7 +389,7 @@ int main(int argc, const char **argv)
     // sanity check the CPU reference code
     for (int j = 0; j < sizeof(test_inputs) / sizeof(uint32_t); ++j)
     {
-        float fres = cast_p3109_signed_nearest_cpu<false>(BITS_TO_FLOAT(&test_inputs[j]), 3);
+        float fres = cast_p3109_signed_nearest_cpu(BITS_TO_FLOAT(&test_inputs[j]), 3, subnormals);
         uint32_t res = FLOAT_TO_BITS(&fres);
         if (res != test_outputs[j])
         {
