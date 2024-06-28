45bfd024f18a78f9f29e9b53a435d2903e2f0dce
Nazar Misyats <nazar.misyats@gmail.com>
2024-06-28 15:58:22 -0700

    Added p3109 quantization benchmark

---
+++ dev/profiling/quantize_p3109.py
@@ -0,0 +1,33 @@
+from torch.profiler import profile, record_function, ProfilerActivity
+import torch
+from mptorch.quant import p3109_quantize
+import argparse
+
+
+# Select matrix sizes and float format (command line arguments)
+parser = argparse.ArgumentParser()
+parser.add_argument("-m", type=int, default=1000)
+parser.add_argument("-n", type=int, default=1000)
+parser.add_argument("-p", type=int, default=3)
+parser.add_argument("--rounding", type=str, default="nearest", choices=["nearest", "stochastic", "troncate"])
+parser.add_argument("--saturation", type=str, choices=["saturate", "overlow", "no_overflow"], default="saturate")
+parser.add_argument("--subnormals", action="store_true", default=False)
+parser.add_argument("--unsigned", action="store_true", default=False)
+args = parser.parse_args()
+
+
+x = torch.rand(args.m, args.n, device="cuda")*10
+
+with profile(activities=[ProfilerActivity.CUDA]) as prof:
+    with record_function("p3109_quantize"):
+        p3109_quantize(
+            x,
+            P=args.p,
+            rounding=args.rounding,
+            saturation_mode=args.saturation,
+            is_signed=not args.unsigned,
+            subnormals=args.subnormals,
+            prng_bits=23
+        )
+
+print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
