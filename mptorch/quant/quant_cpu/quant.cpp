ca50d9de5bf96b200fb826bf6e3cbdfbb8baa07c
Nazar Misyats <nazar.misyats@gmail.com>
2024-06-28 17:07:24 -0700

    Removed commented float_quantize and renamed test_softmax into test_qsoftmax

--- mptorch/quant/quant_cpu/quant.cpp
+++ mptorch/quant/quant_cpu/quant.cpp
@@ -433,8 +433,6 @@ Tensor superfp_quantize_nearest(Tensor a, int man_bits, int exp_bits,
 }
 
 Tensor float_quantized_softmax_nearest(Tensor a, int man_bits, int exp_bits, bool subnormals, bool saturate, int dim){
-  //a = float_quantize(a, man_bits, exp_bits, rNearest, subnormals, saturate);
-
   auto a_array = a.data_ptr<float>();
   auto o = zeros_like(a);
   auto o_array = o.data_ptr<float>();
@@ -488,8 +486,6 @@ Tensor float_quantized_softmax_nearest(Tensor a, int man_bits, int exp_bits, boo
 }
 
 Tensor float_quantized_softmax_lse_nearest(Tensor a, int man_bits, int exp_bits, bool subnormals, bool saturate, int dim){
-  //a = float_quantize(a, man_bits, exp_bits, rNearest, subnormals, saturate);
-
   auto a_array = a.data_ptr<float>();
   auto o = zeros_like(a);
   auto o_array = o.data_ptr<float>();
