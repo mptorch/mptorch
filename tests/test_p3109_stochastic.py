1602f4e5629aa403df2cd3f4b1cc457902df79ab
Victor <victorravain@hotmail.fr>
2024-06-28 17:27:18 -0700

    [FIX] Formated the test cases to be apparently "clearer"

--- tests/test_p3109_stochastic.py
+++ tests/test_p3109_stochastic.py
@@ -137,7 +137,9 @@ def test_p3109_signed_constant():
         
 
 def test_p3109_signed_stochastic_constant():
-
+    if no_cuda():
+        return
+    
     P = 5
 
     values = [0]
@@ -154,6 +156,9 @@ def test_p3109_signed_stochastic_constant():
             assert result[x].item() == i
 
 def test_p3109_signed_stochastic_all_vals():
+    if no_cuda():
+        return
+    
     # for P = 5
     P = 5
     values = []
