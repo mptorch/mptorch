00fc413239dc6551a6fa46559d3f15ea347594d2
Nazar Misyats <nazar.misyats@gmail.com>
2024-07-04 15:48:45 -0700

    Updated softmax cuda test with new syntax

--- dev/cuda/softmax.cu
+++ dev/cuda/softmax.cu
@@ -5,7 +5,7 @@ Compile example:
 nvcc -O3 softmax.cu -o softmax
 
 Run with:
-./softmax [dim]
+./softmax [dim=0|1|2]
 */
 
 
@@ -14,7 +14,7 @@ Run with:
 #include "common.h"
 
 
-struct TensorStrides
+struct DimStrides
 {
     int outer_size;
     int inner_size;
@@ -24,128 +24,129 @@ struct TensorStrides
 };
 
 /* Helper function for tensor striding */
-void tensor_strides(const int *dims, int n_dims, int dim, TensorStrides &strides) {
-  // dimension we are iterating across, needed for negative dims
-  int real_dim = (n_dims + (dim % n_dims)) % n_dims;
-  
-  // columns to iterate over
-  strides.outer_size = 1;
-  for (int i = 0; i < real_dim; ++i) {
-    strides.outer_size *= dims[i];
-  }
-
-  // rows to iterate over
-  strides.inner_size = 1;
-  for (int i = real_dim + 1; i < n_dims; ++i) {
-    strides.inner_size *= dims[i];
-  }
-
-  // size of the dimesnion we are iterating accross
-  strides.dim_stride = strides.inner_size;
-
-  // how much to iterate to get to next set of elements in dim
-  int dim_size = dims[real_dim];
-  strides.dim_size = dim_size;
-  strides.outer_stride = dim_size * strides.dim_stride;
+DimStrides dim_striding(const int *dims, int n_dims, int dim) {
+    DimStrides strides;
+    int real_dim = (n_dims + (dim % n_dims)) % n_dims;
+    strides.outer_size = 1;
+    strides.dim_size = dims[real_dim];
+    strides.inner_size = 1;
+    for (int i = 0; i < real_dim; ++i) {
+        strides.outer_size *= dims[i];
+    }
+    for (int i = real_dim + 1; i < n_dims; ++i) {
+        strides.inner_size *= dims[i];
+    }
+    strides.dim_stride = strides.inner_size;
+    strides.outer_stride = strides.dim_size * strides.dim_stride;
+    return strides;
 }
 
 // ---------------------------------------------------------------------------------------
 /* Host (CPU) implementation of a simple softmax */
-static void softmax_cpu(const float *input, float *output, const int *dims, int n_dims, int dim) {
-    TensorStrides strides;
-    tensor_strides(dims, n_dims, dim, strides);
+static void softmax_cpu(float *input_array, float *output_array, const int *dims, int n_dims, int dim) {
+    auto strides = dim_striding(dims, n_dims, dim);
 
-    for (int L = 0; L < strides.outer_size * strides.inner_size; ++L) {
-        int i = L / strides.inner_size;
-        int j = L % strides.inner_size;
+    for (int i = 0; i < strides.outer_size * strides.inner_size; ++i) {
+        int outer_idx = i / strides.inner_size;
+        int inner_idx = i % strides.inner_size;
 
-        // get max value of the current elements softmax is done on,
-        // this is for ensuring the exponentials don't overflow
-        float max = input[(i*strides.outer_stride) + j];
-        for (int k = 0; k < strides.dim_size; ++k) {
-            int idx = (i*strides.outer_stride) + (k*strides.dim_stride) + j;
+        float* input = input_array + outer_idx * strides.outer_stride + inner_idx;
+        float* output = output_array + outer_idx * strides.outer_stride + inner_idx;
+
+        float max = input[0];
+        for (int k = 1; k < strides.dim_size; ++k) {
+            int idx = k * strides.dim_stride;
             if (input[idx] > max) {
                 max = input[idx];
             }
         }
 
         float sum = 0.0f;
-        // calculates the sum of exponentials and store the intermediate exponentials
         for (int k = 0; k < strides.dim_size; ++k) {
-            int idx = (i*strides.outer_stride) + (k*strides.dim_stride) + j;
+            int idx = k * strides.dim_stride;
             float exp_x = expf(input[idx] - max);
             output[idx] = exp_x;
             sum += exp_x;
         }
-        // Divides all outputs by the current sum
         for (int k = 0; k < strides.dim_size; ++k) {
-            int idx = (i*strides.outer_stride) + (k*strides.dim_stride) + j;
-            output[idx] /= sum;
+            int idx = k * strides.dim_stride;
+            output[idx] = output[idx] / sum;
         }
     }
 }
 
-
-__global__ void softmax_kernel(const float *input, float *output, int inner_size, int outer_stride, int dim_size, int dim_stride, int N) {
+// ---------------------------------------------------------------------------------------
+/* Device (CUDA) implementations of softmax */
+__global__ void softmax_kernel(float *input_array, float *output_array, DimStrides *strides, int N) {
     int id = blockIdx.x * blockDim.x + threadIdx.x;
-    int i = id / inner_size;
-    int j = id % inner_size;
+    int outer_idx = id / strides->inner_size;
+    int inner_idx = id % strides->inner_size;
 
-    if(i*outer_stride + j > N) return;
+    int base_index = outer_idx * strides->outer_stride + inner_idx;
+    if(base_index >= N) return;
 
-    float max = input[i*outer_stride + j];
-    for (int k = 0; k < dim_size; ++k) {
-        int idx = (i*outer_stride) + (k*dim_stride) + j;
+    float* input = input_array + base_index;
+    float* output = output_array + base_index;
+
+    float max = input[0];
+    for (int k = 1; k < strides->dim_size; ++k) {
+        int idx = k * strides->dim_stride;
         if (input[idx] > max) {
             max = input[idx];
         }
     }
 
     float sum = 0.0f;
-    for (int k = 0; k < dim_size; ++k) {
-        int idx = (i*outer_stride) + (k*dim_stride) + j;
+    for (int k = 0; k < strides->dim_size; ++k) {
+        int idx = k * strides->dim_stride;
         float exp_x = expf(input[idx] - max);
         output[idx] = exp_x;
         sum += exp_x;
     }
-    for (int k = 0; k < dim_size; ++k) {
-        int idx = (i*outer_stride) + (k*dim_stride) + j;
-        output[idx] /= sum;
+    for (int k = 0; k < strides->dim_size; ++k) {
+        int idx = k * strides->dim_stride;
+        output[idx] = output[idx] / sum;
     }
 }
 
-void softmax_cuda(const float *input, float *output, const int *dims, int n_dims, int dim, int threads_per_block) {
-    TensorStrides strides;
-    tensor_strides(dims, n_dims, dim, strides);
-    int num_threads = strides.outer_size * strides.inner_size * strides.dim_size;
-    int blocks = num_threads / threads_per_block + (num_threads % threads_per_block != 0);
-    softmax_kernel<<<blocks, threads_per_block>>>(input, output, strides.inner_size, strides.outer_stride, strides.dim_size, strides.dim_stride, num_threads);
+void softmax_cuda(float *input, float *output, const int *dims, int n_dims, int dim, int block_size) {
+    auto h_strides = dim_striding(dims, n_dims, dim);
+    DimStrides *d_strides;
+    cudaCheck(cudaMalloc(&d_strides, sizeof(DimStrides)));
+    cudaCheck(cudaMemcpy(d_strides, &h_strides, sizeof(DimStrides), cudaMemcpyHostToDevice));
+    int N = h_strides.outer_size * h_strides.inner_size * h_strides.dim_size;
+    int blocks = N / block_size + (N % block_size != 0);
+    softmax_kernel<<<blocks, block_size>>>(input, output, d_strides, N);
 }
 
 // ---------------------------------------------------------------------------------------
 int main(int argc, const char **argv) {
     setup_main();
 
-    // create host tensors
     const int dims[] = {100, 50, 259};
     const int n_dims = sizeof(dims)/sizeof(dims[0]);
-    int size = 1;
-    for(int i = 0; i < n_dims; i++) {
-        size *= dims[i];
-    }
-    float *h_input = make_random_float(size);
-    float* h_output = make_zeros_float(size);
-
-    // create cuda tensors
-    float *d_input, *d_output;
-    cudaMalloc(&d_input, size * sizeof(float));
-    cudaMalloc(&d_output, size * sizeof(float));
-    cudaMemcpy(d_input, h_input, size * sizeof(float), cudaMemcpyHostToDevice);
 
     int dim = 1;
     if (argc > 1) {
         dim = atoi(argv[1]);
     }
+    if(dim <= -n_dims-1 || dim >= n_dims) {
+        exit(1);
+    }
+
+    // create host tensors
+    int N = 1;
+    for(int i = 0; i < n_dims; i++) {
+        N *= dims[i];
+    }
+    float *h_input = make_random_float(N);
+    float* h_output = make_zeros_float(N);
+
+    // create cuda tensors
+    float *d_input, *d_output;
+    cudaCheck(cudaMalloc(&d_input, N * sizeof(float)));
+    cudaCheck(cudaMalloc(&d_output, N * sizeof(float)));
+    cudaCheck(cudaMemcpy(d_input, h_input, N * sizeof(float), cudaMemcpyHostToDevice));
 
     // cpu reference
     softmax_cpu(h_input, h_output, dims, n_dims, dim);
@@ -156,7 +157,7 @@ int main(int argc, const char **argv) {
         printf("Checking block size %d.\n", block_size);
         softmax_cuda(d_input, d_output, dims, n_dims, dim, block_size);
         float tol = 1e-7f;
-        validate_result(d_output, h_output, "softmax output", size, tol);
+        validate_result(d_output, h_output, "softmax output", N, tol);
     }
 
     printf("All results match. Starting benchmarks.\n\n");
@@ -168,7 +169,7 @@ int main(int argc, const char **argv) {
 
         // estimate memory bandwidth achieved
         // for each output element, we do 3 reads and 2 writes, 4 bytes each
-        long memory_ops = size * 5 * (int)sizeof(float);
+        long memory_ops = N * 5 * (int)sizeof(float);
         float memory_bandwidth = memory_ops / elapsed_time / 1e6;
 
         printf("block_size %4d | time %.4f ms | bandwidth %.2f GB/s\n", block_size, elapsed_time, memory_bandwidth);
@@ -178,6 +179,6 @@ int main(int argc, const char **argv) {
     free(h_input);
     free(h_output);
     
-    cudaFree(d_input);
-    cudaFree(d_output);
+    cudaCheck(cudaFree(d_input));
+    cudaCheck(cudaFree(d_output));
 }
