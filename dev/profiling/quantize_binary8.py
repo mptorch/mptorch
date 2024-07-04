0ed668c1d527b98736dca86d01514d1a5fd0f103
Nazar Misyats <nazar.misyats@gmail.com>
2024-07-03 19:58:06 -0700

    Added CUDA availability check in quantize_binary8 benchmark

--- dev/profiling/quantize_binary8.py
+++ dev/profiling/quantize_binary8.py
@@ -16,18 +16,20 @@ parser.add_argument("--unsigned", action="store_true", default=False)
 args = parser.parse_args()
 
 
-x = torch.rand(args.m, args.n, device="cuda")*10
+if torch.cuda.is_available():
+    x = torch.rand(args.m, args.n, device="cuda")*10
 
-with profile(activities=[ProfilerActivity.CUDA]) as prof:
-    with record_function("binary8_quantize"):
-        binary8_quantize(
-            x,
-            P=args.p,
-            rounding=args.rounding,
-            saturation_mode=args.saturation,
-            is_signed=not args.unsigned,
-            subnormals=args.subnormals,
-            prng_bits=23
-        )
-
-print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
+    with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA]) as prof:
+        with record_function("binary8_quantize"):
+            binary8_quantize(
+                x,
+                P=args.p,
+                rounding=args.rounding,
+                saturation_mode=args.saturation,
+                is_signed=not args.unsigned,
+                subnormals=args.subnormals,
+                prng_bits=23
+            )
+    print(prof.key_averages().table(sort_by="cuda_time_total", row_limit=10))
+else:
+    raise NotImplementedError("No CUDA-capable device found. Stopping script.")
