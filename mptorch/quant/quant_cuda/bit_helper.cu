0688fe5a4943449695b2bdebf67e3de8f768b7fb
John Kim <johnnkimk@gmail.com>
2024-07-10 11:47:26 -0700

    annotated code from meeting w/ silviu (for victor and john)

--- mptorch/quant/quant_cuda/bit_helper.cu
+++ mptorch/quant/quant_cuda/bit_helper.cu
@@ -216,16 +216,28 @@ binary8_clip_exponent(int exp_bits, int man_bits, uint32_t old_num, uint32_t qua
       } else {
         quantized_num = 0;
       }
-    } 
-    if(!subnormal) {
-      uint32_t min_num = (uint32_t)min_exponent_store << 23;
-      uint32_t middle_num = ((uint32_t)(min_exponent_store - 1) << 23);
-      if ((old_num & 0x7FFFFFFF) > middle_num) {
-        quantized_num = old_sign | min_num;
-      } else {
-        quantized_num = 0;
-      }
     }
+
+    // this is how subnormals will be represented when there are no subnormals instead of RNE: 2 ^ (-bias) * 1.M --> ONLY CHANGE WOULD BE INCREASEING EXPONENT RANGE BY 1 FOR MINIMUM
+    // middle point: 2^(-BIAS - 1) * 1.00000000 --> round to 0 or 2^(-BIAS)
+
+    // The following is the original implementation where RNE was conducted when there were no subnormals
+    // if(!subnormal) {
+    //   uint32_t min_num = (uint32_t)min_exponent_store << 23;
+    //   uint32_t middle_num = ((uint32_t)(min_exponent_store - 1) << 23);
+    //   if ((old_num & 0x7FFFFFFF) > middle_num) {
+    //     quantized_num = old_sign | min_num;
+    //   } else {
+    //     quantized_num = 0;
+    //   }
+    // }
+
+    // The following is the new implementation where subnormal values are normalized where there are no subnormals
+    // min exp is actually (current min exp - 1) so min exp (which should be normal) is actually normalized
+    // treat as if 1.man, not 0.man for subnormals -> normal range is now increased by 1 to handle that subnormal case
+    if (!subnormal){
+         
+    } 
   }
   return quantized_num;
 }
