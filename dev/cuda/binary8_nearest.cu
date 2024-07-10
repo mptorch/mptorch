ced257fa8861cb57bcbd10ad3aedf357864d5ca2
Victor <victorravain@hotmail.fr>
2024-07-10 10:29:52 -0700

    [FIX] fixing in cuda test binary8 nearest

--- dev/cuda/binary8_nearest.cu
+++ dev/cuda/binary8_nearest.cu
@@ -101,7 +101,7 @@ uint32_t round_bitwise_nearest_cpu(uint32_t target, int man_bits) {
   uint32_t machine_eps = 1 << (22 - man_bits);
   int offset = (down == machine_eps);
   uint32_t add_r = target + machine_eps;
-  return add_r & ~((1 << (23 - man_bits + offset)) - 1);
+  return add_r & ~((1 << std::min((23 - man_bits + offset),23)) - 1);
 }
 
 uint32_t round_bitwise_nearest_p1_cpu(uint32_t target, int man_bits) {
