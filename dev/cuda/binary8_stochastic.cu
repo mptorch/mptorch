a74e0e9536242f82188ae623b338b458b6d1d389
John Kim <johnnkimk@gmail.com>
2024-07-10 11:20:19 -0700

    [FIX] Reverted binary8 naming to p3109 (requires a change in gfloat), fixed prng_bits logic in CUDA test

--- dev/cuda/binary8_stochastic.cu
+++ dev/cuda/binary8_stochastic.cu
@@ -127,7 +127,8 @@ float cast_binary8_signed_stochastic_cpu(float origin_float, int P, uint32_t ran
     }
 
     rand_prob = rand_prob << 9 >> 9;
-    rand_prob = rand_prob & ~(1 << (23 - prng_bits) - 1);
+    // rand_prob = rand_prob & ~(1 << (23 - prng_bits) - 1);
+    rand_prob = rand_prob & ~(1 << (23 - man_bits - prng_bits) - 1);
 
     uint32_t uval8 = round_bitwise_stochastic_cpu(uval32, rand_prob, man_bits - subnormal_shift);
     uval8 = binary8_clip_exponent_cpu(exp_bits, man_bits, uval32, uval8, saturation_mode, subnormals);
@@ -162,7 +163,8 @@ float cast_binary8_unsigned_stochastic_cpu(float origin_float, int P, uint32_t r
     }
 
     rand_prob = rand_prob << 9 >> 9;
-    rand_prob = rand_prob & ~(1 << (23 - prng_bits) - 1);
+    // rand_prob = rand_prob & ~(1 << (23 - prng_bits) - 1);
+    rand_prob = rand_prob & ~(1 << (23 - man_bits - prng_bits) - 1);
 
     uint32_t uval8 = round_bitwise_stochastic_cpu(uval32, rand_prob, man_bits - subnormal_shift);
     uval8 = binary8_clip_exponent_cpu(exp_bits, man_bits, uval32, uval8, saturation_mode, subnormals);
@@ -291,7 +293,8 @@ __device__ float cast_binary8_signed_stochastic(float origin_float, int P, uint3
     }
 
     rand_prob = rand_prob << 9 >> 9;
-    rand_prob = rand_prob & ~(1 << (23 - prng_bits) - 1);
+    // rand_prob = rand_prob & ~(1 << (23 - prng_bits) - 1);
+    rand_prob = rand_prob & ~(1 << (23 - man_bits - prng_bits) - 1);
 
     uint32_t uval8 = round_bitwise_stochastic(uval32, rand_prob, man_bits - subnormal_shift);
     uval8 = binary8_clip_exponent(exp_bits, man_bits, uval32, uval8, saturation_mode, subnormals);
@@ -326,7 +329,8 @@ __device__ float cast_binary8_unsigned_stochastic(float origin_float, int P, uin
     }
 
     rand_prob = rand_prob << 9 >> 9;
-    rand_prob = rand_prob & ~(1 << (23 - prng_bits) - 1);
+    // rand_prob = rand_prob & ~(1 << (23 - prng_bits) - 1);
+    rand_prob = rand_prob & ~(1 << (23 - man_bits - prng_bits) - 1);
 
     uint32_t uval8 = round_bitwise_stochastic(uval32, rand_prob, man_bits - subnormal_shift);
     uval8 = binary8_clip_exponent(exp_bits, man_bits, uval32, uval8, saturation_mode, subnormals);
