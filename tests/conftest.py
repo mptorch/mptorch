46a87a9aa457ff1bf11a47651f4b2f07e2b123ba
Nazar Misyats <nazar.misyats@gmail.com>
2024-07-18 10:37:26 -0700

    Added custom and automatic seed parametrization

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,12 @@ import torch
 import pytest
 
 @pytest.fixture(scope="function", autouse=True)
-def determinism():
-    seed = 1234
-    torch.manual_seed(seed)
-    torch.cuda.manual_seed(seed)
+def seed(request):
+    if hasattr(request, "param"):
+        value = request.param
+    else:
+        value = 1234  # default seed
+    torch.manual_seed(value)
+    torch.cuda.manual_seed(value)
     torch.backends.cudnn.deterministic = True
+    return value
--- tests/markers.py
+++ tests/markers.py
@@ -1,6 +1,9 @@
 import pytest
 import torch
 
+def parametrize_seed(*seeds):
+    return pytest.mark.parametrize("seed", seeds, indirect=True)
+
 requires_cuda = pytest.mark.skipif(
     not torch.cuda.is_available(),
     reason='No CUDA-capable device found.'
