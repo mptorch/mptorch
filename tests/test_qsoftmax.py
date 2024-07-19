f89bc3616873df5045cdba516e61186dd6b08753
Nazar Misyats <nazar.misyats@gmail.com>
2024-07-19 11:48:53 -0700

    Updated softmax tests

--- tests/test_qsoftmax.py
+++ tests/test_qsoftmax.py
@@ -11,14 +11,14 @@ from tests.markers import available_devices
 
 @pytest.fixture(scope="module")
 def fp_format():
-    return FloatingPoint(exp=8, man=23, subnormals=True, saturate=False)
+    return FloatingPoint(exp=5, man=10, subnormals=True, saturate=False)
 
 @pytest.fixture(scope="module")
-def quant_fp():
+def quant_fp(fp_format):
     return lambda x: float_quantize(
         x,
-        exp=8,
-        man=23,
+        exp=fp_format.exp,
+        man=fp_format.man,
         rounding="nearest",
         subnormals=True,
         saturate=False,
@@ -71,48 +71,12 @@ def test_softmax_formats(formats_lse, formats_div):
 @pytest.mark.parametrize("shape", [(20, 30, 40)])
 @pytest.mark.parametrize("dim", [0, 1, 2])
 @pytest.mark.parametrize("fmt", ["div", "lse"])
-def test_softmax_forward_backward(device, shape, dim, fmt, formats_div, formats_lse):
+def test_custom_softmax(device, shape, dim, fmt, formats_div, formats_lse):
     if fmt == "div":
         formats = formats_div
     else:
         formats = formats_lse
     
-    x_ref = torch.rand(*shape, device=device, requires_grad=True)
-    x_res = x_ref.clone().detach()
-    x_res.requires_grad_(True)
-
-    y_ref = torch.softmax(x_ref, dim=dim)
-    y_res = Q.qsoftmax(x_res, dim, formats)
-    torch.testing.assert_close(y_res, y_ref, atol=1e-5, rtol=0)
-
-    grad = torch.rand(*shape, device=device)
-    y_ref.backward(grad)
-    y_res.backward(grad)
-    torch.testing.assert_close(x_res.grad, x_ref.grad, atol=1e-5, rtol=0)
-
-@pytest.mark.parametrize("device", available_devices)
-@pytest.mark.parametrize("shape", [(20, 30, 40)])
-@pytest.mark.parametrize("dim", [2])
-def test_softmax_layer_forward_backward(device, shape, dim):
-    man, exp = 12, 8
-    quant_fp = lambda x: Q.float_quantize(
-        x, exp=exp, man=man, rounding="nearest", subnormals=True, saturate=False
-    )
-    fp_format = FloatingPoint(exp=exp, man=man, subnormals=True, saturate=False)
-    formats = QSoftmaxFormats(
-        fwd_exp=fp_format,
-        fwd_off=fp_format,
-        fwd_acc=fp_format,
-        fwd_div=fp_format,
-
-        bwd_add=fp_format,
-        bwd_mul=fp_format,
-        bwd_div=fp_format,
-
-        output_quant=quant_fp,
-        input_quant=quant_fp,
-        grad_quant=quant_fp,
-    )
     layer = torch.nn.Softmax(dim)
     qlayer = QSoftmax(dim, formats)
 
@@ -122,9 +86,29 @@ def test_softmax_layer_forward_backward(device, shape, dim):
 
     y_ref = layer.forward(x_ref)
     y_res = qlayer.forward(x_res)
-    torch.testing.assert_close(y_res, y_ref, atol=1e-3, rtol=0)
+    torch.testing.assert_close(y_res, y_ref, atol=1e-3, rtol=0.0)
 
     grad = torch.rand(*shape, device=device)
     y_ref.backward(grad)
     y_res.backward(grad)
-    torch.testing.assert_close(x_res.grad, x_ref.grad, atol=1e-3, rtol=0)
+    torch.testing.assert_close(x_res.grad, x_ref.grad, atol=1e-3, rtol=0.0)
+
+@pytest.mark.parametrize("device", available_devices)
+@pytest.mark.parametrize("shape", [(20, 30, 40)])
+@pytest.mark.parametrize("dim", [2])
+def test_default_softmax(device, shape, dim):
+    layer = torch.nn.Softmax(dim)
+    qlayer = QSoftmax(dim, QSoftmaxFormats())
+
+    x_ref = torch.rand(*shape, device=device, requires_grad=True)
+    x_res = x_ref.clone().detach()
+    x_res.requires_grad_(True)
+
+    y_ref = layer.forward(x_ref)
+    y_res = qlayer.forward(x_res)
+    torch.testing.assert_close(y_res, y_ref, atol=1e-8, rtol=0.0)
+
+    grad = torch.rand(*shape, device=device)
+    y_ref.backward(grad)
+    y_res.backward(grad)
+    torch.testing.assert_close(x_res.grad, x_ref.grad, atol=1e-8, rtol=0.0)
