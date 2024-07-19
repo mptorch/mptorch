f67fbc4ba27d9337964326a4ab8f69770d7bcb9e
Silviu Filip <silviu.ioan.filip@gmail.com>
2024-07-19 05:29:29 -0700

    fix for BWD call error

--- mptorch/quant/quant_function.py
+++ mptorch/quant/quant_function.py
@@ -621,7 +621,7 @@ def mp_bmm(a, b, formats, use_forward=True):
             formats.bwd_fma,
             formats.bwd_rnd,
         )
-    if type(formats.fwd_add) == FloatingPoint:
+    if type(add_cfg) == FloatingPoint:
         return float_bmm(
             a,
             b,
@@ -634,7 +634,7 @@ def mp_bmm(a, b, formats, use_forward=True):
             subnormals=add_cfg.subnormals,
             saturate=add_cfg.saturate,
         )
-    elif type(formats.fwd_add) == SuperNormalFloat:
+    elif type(add_cfg) == SuperNormalFloat:
         return superfp_bmm(
             a,
             b,
