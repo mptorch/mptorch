95354b67fbe0541a53f31dfadc4bec7c37164303
John Kim <johnnkimk@gmail.com>
2024-07-23 09:23:20 -0700

    [FIX] corrected overflow policies in enum

--- mptorch/number.py
+++ mptorch/number.py
@@ -198,7 +198,7 @@ class Binary8(Number):
         assert type(P) == input and 8 > P > 0, "invalid input for precision: {}".format(P)  # is P = 8 valid?
         assert type(signed) == bool, "invalid type for signed or unsigned choice: {}".format(type(signed))
         assert type(subnormals) == bool, "invalid type for allowing subnormals or not: {}".format(type(subnormals))
-        assert type(overflow_policy) == str and overflow_policy in ["saturate", "overflow", "no_overflow"], "invalid input for saturation mode: {}".format(overflow_policy)
+        assert type(overflow_policy) == str and overflow_policy in ["saturate_infty", "saturate_maxfloat", "saturate_maxfloat2"], "invalid input for saturation mode: {}".format(overflow_policy)
 
         self.P = P
         spec_exp = P == 1
@@ -224,7 +224,7 @@ class Binary8(Number):
             self.normal_min = (1 + 2**-self.man) * (2**(min_exp - 1))
     
         if signed:
-            if overflow_policy == "no_overflow":    # no inf case, so max is FF not FE
+            if overflow_policy == "saturate_maxfloat2":    # no inf case, so max is FF not FE
                 if self.man > 0:
                     self.normal_max = (2 - 2 **-self.man) * (2**max_exp)    # good for more than 0 mantissa 
                 else:
@@ -235,7 +235,7 @@ class Binary8(Number):
                 else:
                     self.normal_max = 2**max_exp    # 0 mantissa case
         else:   # unsigned case
-            if overflow_policy == "no_overflow":    # no inf case, so max is FE not FD
+            if overflow_policy == "saturate_maxfloat2":    # no inf case, so max is FE not FD
                 if self.man > 0:
                     self.normal_max = (2 - 2**-(self.man-1)) * (2**max_exp)    # good for more than 0 mantissa 
                 else:
