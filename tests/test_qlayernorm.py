51ec25f3b0b6cbe7ca762834fb0aeea8b8b61e8f
Robert Sherrick <boblelobster4@gmail.com>
2024-07-22 18:12:27 -0700

    updated tests with backward pass

--- tests/test_qlayernorm.py
+++ tests/test_qlayernorm.py
@@ -1,13 +1,10 @@
 import torch
+import pytest
 from torch.nn import functional as F
 from mptorch import FloatingPoint
 from mptorch.quant import QAffineFormats
 from mptorch.quant import float_quantize
-from mptorch.quant import functional as Q
-
-device = "cpu"
-seed = 1234
-torch.manual_seed(seed)
+from mptorch.quant import functional as Q 
 
 fp_format = FloatingPoint(
     exp=8, man=23, subnormals=True, saturate=False
@@ -34,25 +31,26 @@ layer_formats = QAffineFormats(
     bias_quant=quant_fp,
 )
 
-def test_qlayer_norm_forward():
-    a = torch.randn(20, 30, 40, device=device)
-    ref = F.layer_norm(a, [a.size(-2), a.size(-1)], weight=None, bias=None, eps=1e-5)
-    res = Q.qlayernorm(a, [a.size(-2), a.size(-1)], weight=None, bias=None, eps=1e-5, formats=layer_formats)
+@pytest.mark.parametrize("normalized_shape", [[30, 40], [40]])
+def test_qlayer_norm_forward(normalized_shape):
+    a = torch.randn(20, 30, 40, device="cpu")
+    ref = F.layer_norm(a, normalized_shape, weight=None, bias=None, eps=1e-5)
+    res = Q.qlayernorm(a, normalized_shape, weight=None, bias=None, eps=1e-5, formats=layer_formats)
 
     torch.testing.assert_close(ref, res, atol=1e-5, rtol=0)
 
-    
-# def test_qlayer_norm_backward():
-#     a = torch.randn(10, 30, 40, 20, device=device)
+@pytest.mark.parametrize("normalized_shape", [[30, 40], [40]])
+def test_qlayer_norm_backward(normalized_shape):
+    a = torch.randn(20, 30, 40, device="cpu")
 
-#     ref1 = torch.randn(10, 30, 40, 20, device=device, requires_grad=True)
-#     res1 = ref1.clone().detach()
-#     res1.requires_grad_(True)
+    ref_x = torch.randn(20, 30, 40, device="cpu", requires_grad=True)
+    res_x = ref_x.clone().detach()
+    res_x.requires_grad_(True)
 
-#     ref2 = F.layer_norm(ref1, ref1.size(-1), )
-#     res2 = Q.qlayernorm(res1, 1, layer_formats, weight=None, bias)
+    ref_y = F.layer_norm(ref_x, normalized_shape, weight=None, bias=None, eps=1e-5)
+    res_y = Q.qlayernorm(res_x, normalized_shape, weight=None, bias=None, eps=1e-5, formats=layer_formats)
 
-#     ref2.backward(a)
-#     res2.backward(a)
+    ref_y.backward(a)
+    res_y.backward(a)
 
-#     torch.testing.assert_close(ref1.grad, res1.grad, atol=1e-5, rtol=0)
+    torch.testing.assert_close(ref_x.grad, res_x.grad, atol=1e-5, rtol=0)
