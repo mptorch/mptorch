fdf17b37666e29828a8bba5691469a88b384236f
Nazar Misyats <nazar.misyats@gmail.com>
2024-07-25 12:14:38 -0700

    Added accuracy test for scaled linear

--- tests/test_scaling.py
+++ tests/test_scaling.py
@@ -1,7 +1,9 @@
 from mptorch.quant import QAffineFormats
 from mptorch import FloatingPoint, FixedPoint
 from mptorch.quant.functional import qlinear, qmm, qmatmul
+from mptorch.quant import QLinear
 import torch
+from torch.testing import assert_close
 from unittest.mock import MagicMock, PropertyMock
 import pytest
 
@@ -82,3 +84,41 @@ def test_scaling_is_used(device, mm):
     normal_max_input.assert_called_once()
     normal_max_weight.assert_called_once()
     normal_max_grad.assert_called_once()
+
+
+@pytest.mark.parametrize("device", ["cpu"])
+@pytest.mark.parametrize("exp,man", [(8,23), (5,10), (4,3)])
+def test_qlinear_custom_mm_scaled(device, exp, man):
+    mac_format = FloatingPoint(exp=8, man=23, subnormals=True, saturate=False)
+    fp_format = FloatingPoint(exp=exp, man=man, subnormals=True, saturate=False)
+    formats_q = QAffineFormats(
+        fwd_mac=(mac_format,),
+        bwd_mac=(mac_format,),
+        fwd_rnd="nearest",
+        bwd_rnd="nearest",
+        weight_quant=(fp_format, "nearest"),
+        grad_quant=(fp_format, "nearest"),
+        output_quant=(fp_format, "nearest"),
+        input_quant=(fp_format, "nearest"),
+        bias_quant=(fp_format, "nearest")
+    )
+    x = torch.randn(11, 1034)
+    m = torch.nn.Linear(1034, 542, bias=True)
+    qm = QLinear(1034, 542, formats=formats_q, bias=True)
+    m = m.to(device)
+    qm = qm.to(device)
+    x = x.to(device)
+    qx = x.clone().detach()
+    m.weight.data = qm.weight.data.clone().detach()
+    m.bias.data = qm.bias.data.clone().detach()
+
+    res_m = m(x)
+    res_qm = qm(qx)
+    assert_close(res_m, res_qm, atol=1e-1, rtol=0.0)
+
+    res_m = m(x).mean()
+    res_m.backward()
+    res_qm = qm(qx).mean()
+    res_qm.backward()
+    assert_close(m.bias.grad, qm.bias.grad, atol=1e-1, rtol=0.0)
+    assert_close(m.weight.grad, qm.weight.grad, atol=1e-1, rtol=0.0)
