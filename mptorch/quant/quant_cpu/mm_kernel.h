e70a681869be534a3afdce31ac1b6b423205775e
Silviu Filip <silviu.ioan.filip@gmail.com>
2025-05-19 19:32:56 +0300

    [improv] CPU bmm implementations

--- mptorch/quant/quant_cpu/mm_kernel.h
+++ mptorch/quant/quant_cpu/mm_kernel.h
@@ -17,6 +17,17 @@ void mm_kernel(float *a, float *b, float *c, int M, int K, int N, Qadd quant_add
             }
 }
 
+template<class Qadd, class Qmul>
+void bmm_kernel(float *a, float *b, float *c, int B, int M, int K, int N, Qadd quant_add, Qmul quant_mul)
+{
+    for (int i = 0; i < B; ++i) {
+        float* ia = a + i * M * K;
+        float* ib = b + i * K * N;
+        float* ic = c + i * K * M;
+        mm_kernel(ia, ib, ic, M, K, N, quant_add, quant_mul);
+    }
+}
+
 template <class Qfma>
 void mm_fma_kernel(float *a, float *b, float *c, int M, int K, int N, Qfma quant_fma)
 {
@@ -24,6 +35,17 @@ void mm_fma_kernel(float *a, float *b, float *c, int M, int K, int N, Qfma quant
         for (int k = 0; k < K; ++k)
             for (int j = 0; j < N; ++j)
                 c[i * N + j] = quant_fma(fmaf(a[i * K + k], b[k * N + j], c[i * N + j]));
+}
+
+template<class Qfma>
+void bmm_fma_kernel(float *a, float *b, float *c, int B, int M, int K, int N, Qfma quant_fma)
+{
+    for (int i = 0; i < B; ++i) {
+        float* ia = a + i * M * K;
+        float* ib = b + i * K * N;
+        float* ic = c + i * K * M;
+        mm_fma_kernel(ia, ib, ic, M, K, N, quant_fma);
+    }
 }
 
 template <class Qadd, class Qmul>
@@ -41,6 +63,16 @@ void mm_kahan_kernel(float *a, float *b, float *c, int M, int K, int N, Qadd qua
             }
 }
 
+template <class Qadd, class Qmul>
+void bmm_kahan_kernel(float *a, float *b, float *c, int M, int K, int N, Qadd quant_add, Qmul quant_mul) {
+    for (int i = 0; i < B; ++i) {
+        float* ia = a + i * M * K;
+        float* ib = b + i * K * N;
+        float* ic = c + i * K * M;
+        mm_kahan_kernel(ia, ib, ic, M, K, N, quant_add, quant_mul);
+    }    
+}
+
 template <class Qfma>
 void mm_kahan_fma_kernel(float *a, float *b, float *c, int M, int K, int N, Qfma quant_fma)
 {
@@ -53,4 +85,15 @@ void mm_kahan_fma_kernel(float *a, float *b, float *c, int M, int K, int N, Qfma
                 float t = quant_fma(quant_fma(t - c[i * N + j]) + y);
                 c[i * N + j] = t;
             }
+}
+
+template <class Qfma>
+void bmm_kahan_fma_kernel(float *a, float *b, float *c, int M, int K, int N, Qfma quant_fma)
+{
+    for (int i = 0; i < B; ++i) {
+        float* ia = a + i * M * K;
+        float* ib = b + i * K * N;
+        float* ic = c + i * K * M;
+        mm_kahan_fma_kernel(ia, ib, ic, M, K, N, quant_fma);
+    }
 }
