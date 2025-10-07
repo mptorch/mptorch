6d7524f5510492d42910e4eefd1ea1cc219f069f
Silviu Filip <silviu.ioan.filip@gmail.com>
2025-10-07 07:12:02 +0200

    [WIP] tests

--- tests/test_binaryK_quantize.py
+++ tests/test_binaryK_quantize.py
@@ -11,10 +11,23 @@ from tests.quant import bits_to_float, float_to_bits, assert_quant
 
 @pytest.mark.parametrize("device", available_devices)
 @pytest.mark.parametrize("K", [4, 6, 8])
-def test_binaryK_signed_to_gfloat(device, K):
-    for P in range(1, K):
+def test_binaryK_to_gfloat_nearest_even_signed(device, K):
+    for P in range(2, K):
         fi = format_info_p3109(K, P, signedness=Signedness.Signed)
         bias = 2 ** (K - P - 1)
         start_value = 2 ** (1 - bias - P - 3)
         istart_value = float_to_bits(start_value)
         increment = 0x7FFFFFFF & (1 << (22 - (P - 1) - 2))
+
+        vals_to_test = [
+            bits_to_float(istart_value + i * increment) for i in range(0, 2 ** (K + 1))
+        ]
+        x = torch.tensor(vals_to_test, dtype=torch.float32).to(device)
+        gqx = x.clone().detach().to("cpu")
+        gqx.apply_(lambda x: round_float(fi, x, rnd=RoundMode.TiesToEven))
+        gqx = gqx.to(device)
+        qx = binaryK_quantize(x, K, P, rounding="nearest_even")
+
+        print(torch.all(qx == gqx))
+
+        assert torch.all(qx == gqx)
