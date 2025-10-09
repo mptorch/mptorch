2eb20727f69cdffad480e9ae7bd8f5102c77653c
sfilip <silviu.ioan.filip@gmail.com>
2025-10-09 20:53:52 +0300

    [FIX] change to documentation workflow

--- tests/test_binaryK_quantize.py
+++ tests/test_binaryK_quantize.py
@@ -21,9 +21,12 @@ from tests.quant import bits_to_float, float_to_bits, assert_quant
         (RoundMode.TowardZero, "zero"),
     ],
 )
-def test_binaryK_to_gfloat_nearest_even_signed(device, K, rounding_mode):
+@pytest.mark.parametrize(
+    "signedness", [(Signedness.Signed, True), (Signedness.Unsigned, False)]
+)
+def test_binaryK_vs_gfloat(device, K, rounding_mode, signedness):
     for P in range(1, K):
-        fi = format_info_p3109(K, P, signedness=Signedness.Signed)
+        fi = format_info_p3109(K, P, signedness=signedness[0])
         bias = 2 ** (K - P - 1)
         start_value = 2 ** (1 - bias - P - 3)
         istart_value = float_to_bits(start_value)
@@ -32,14 +35,21 @@ def test_binaryK_to_gfloat_nearest_even_signed(device, K, rounding_mode):
         vals_to_test = [
             bits_to_float(istart_value + i * increment) for i in range(0, 2 ** (K + 1))
         ]
-        vals_to_test_neg = [-x for x in vals_to_test]
-        vals_to_test = [vals_to_test, vals_to_test_neg]
+        if signedness[1]:
+            vals_to_test_neg = [-x for x in vals_to_test]
+            vals_to_test = [vals_to_test, vals_to_test_neg]
         x = torch.tensor(vals_to_test, dtype=torch.float32).to(device)
         gqx = x.clone().detach().to("cpu")
-        gqx.apply_(lambda x: round_float(fi, x, rnd=rounding_mode[0]))
+        gqx.apply_(
+            lambda x: round_float(
+                fi,
+                x,
+                rnd=rounding_mode[0],
+            )
+        )
         gqx = gqx.to(device)
-        qx = binaryK_quantize(x, K, P, rounding=rounding_mode[1])
-
-        print(torch.all(qx == gqx))
+        qx = binaryK_quantize(
+            x, K, P, rounding=rounding_mode[1], is_signed=signedness[1]
+        )
 
         assert torch.all(qx == gqx)
